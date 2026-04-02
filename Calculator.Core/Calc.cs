e2e7855de8412672bdb44ada405a3674c2b82408
Starinin Andrey <starinin-andrey@ya.ru>
2026-04-02 20:20:51 +0300

    Add docfx

--- Calculator.Core/Calc.cs
+++ Calculator.Core/Calc.cs
@@ -2,15 +2,20 @@
 
 namespace Calculator.Core;
 
+/// <summary>
+/// Класс для вычислений.
+/// </summary>
 public static class Calc
 {
+    /// <summary>
+    /// Вычисляет деление двух чисел.
+    /// </summary>
+    /// <param name="a">Первое число.</param>
+    /// <param name="b">Второй число.</param>
+    /// <returns>Результат деления.</returns>
+    /// <exception cref="DivideByZeroException">Если второе число равно 0.</exception>
     public static double Div(double a, double b)
     {
-        if (b == 0)
-        {
-            throw new DivideByZeroException("Деление на ноль запрещено");
-        }
-        //TODO: Добавить проверку деления
         if (b == 0)
         {
             throw new DivideByZeroException("Деление на ноль запрещено");
