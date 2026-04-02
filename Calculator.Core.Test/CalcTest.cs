b400cabcb068c01bc7e811afa59d436ffb33c67b
Starinin Andrey <starinin-andrey@ya.ru>
2026-04-02 19:00:56 +0300

    Init

---
+++ Calculator.Core.Test/CalcTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xunit;
+
+namespace Calculator.Core.Test;
+
+public class CalcTest
+{
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(-1, -2)]
+    [InlineData(0, 2)]
+    public void Div_PositiveTest(double a, double b)
+    {
+        var expected = a / b;
+        var actual = Calc.Div(a,b);
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Div_NegativeTest()
+    {
+        Assert.Throws<DivideByZeroException>(() =>
+        {
+            Calc.Div(1, 0);
+        });
+    }
+}
